3a99b5f78e9f668095b509a74eb3d25e806d7187
geoffdibb <geoffdibb@yahoo.co.uk>
2019-07-26 09:35:03 +0100

    base framework setup

---
+++ routes/usernameschema.js
@@ -0,0 +1,21 @@
+    
+var mongoose = require('mongoose');
+var Schema  = mongoose.Schema;
+
+var itemSchema = new Schema({
+    username: {
+    type: String,
+    required: true
+    },
+    contents: {
+       type: String,
+        required: true
+    }
+});
+
+var Item = mongoose.model(
+    'items',
+    itemSchema
+);
+
+module.exports = Item;
