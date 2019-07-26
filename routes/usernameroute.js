04daad9ef52356f5ea6715ee945dfc4699f7e80b
geoffdibb <geoffdibb@yahoo.co.uk>
2019-07-26 12:56:50 +0100

    unique email and user now required

--- routes/usernameroute.js
+++ routes/usernameroute.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const _ = require("lodash");
-const Item = require("./usernameschema.js")
-const validateemail = require("./emailvalidation.js");
+const Item = require("./usernameschema")
+const validateemail = require("./emailvalidation");
 const bcrypt = require("bcryptjs");
 
-
+// @route   GET item/username
+// @desc    Show all - password
+// @access  Public
 //show all
 router.get("/all", (req, res) => {
   const errors = {};
@@ -20,32 +22,50 @@ router.get("/all", (req, res) => {
     .catch(err => res.status(404).json({ noItems: "There are nothing at all" }));
 });
 
+
+
 // @route   GET item/username
 // @desc    Get all items from one username
 // @access  Public
-router.post("/username", (req, res) => {
-  const errors = {};
+
+//login2
+router.post("/login", (req, res) => {
+
+  let errors = {};
+
   const password = req.body.password;
-      let hashedpassword;
+  let hashedpassword;
 
-  Item.findOne({ username: req.body.username})
+
+  Item.findOne({ username: req.body.username })
     .then(items => {
-      hashedpassword=items.password;
-      if (!items) {
-        res.status(404).json(errors);
-      }
-bcrypt.compare(password, hashedpassword).then(isMatch => {
-   if (isMatch) {
-      res.status(200).json("Login Valid");
-    console.log("valid login")
-   }
+      hashedpassword = items.password;
 
-    })
-    .catch(err => res.status(404).json(err));
-})});
+      bcrypt.compare(password, hashedpassword).then(isMatch => {
+        if (isMatch) {
 
+          res.status(200).json("Login Valid");
+          console.log("valid login")
 
+            .then(() => {
+              res.json({ success: true });
+            })
+            .catch(err =>
+              res.status(404).json({ usernotfound: "No user found" })
+            );
 
+        } else {
+          return res.status(400).json(errors);
+        }
+      });
+
+    }).catch(err => res.status(404).json({ err }));
+
+});
+
+// @route   Post item/username
+// @desc    Create a new account
+// @access  Public
 //create
 router.post("/createlogin", (req, res) => {
   const { errors, isValid } = validateemail(req.body);
@@ -53,32 +73,38 @@ router.post("/createlogin", (req, res) => {
     return res.status(400).json(errors);
   }
 
-    const item = new Item({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        password2: req.body.password2
+  const item = new Item({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    password2: req.body.password2
   });
- bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.genSalt(10, (err, salt) => {
     bcrypt.hash(item.password, salt, (err, hash) => {
       if (err) throw err;
       item.password = hash;
       item.save().then(item => res.json(item))
-      .catch(err => console.log(err));
-   });
+        .catch(err => console.log(err));
+    });
 
   });
-});
+}
+);
 
 
 
-
+// @route   DELETE item/username
+// @desc    Delete an account on username
+// @access  Public
 //delete
 router.delete("/delete", (req, res) => {
-  Item.deleteOne({'username': req.body.username})
-  .then(({ ok, n }) => {
-              res.json(Item)
-    })})
+  Item.deleteOne({ 'username': req.body.username })
+    .then(({ ok, n }) => {
+      res.json(Item)
+    })
+})
 
 
 module.exports = router;
+
+
